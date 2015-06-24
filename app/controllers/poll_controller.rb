7bd494e1e87aa152d0660db8c904dc25c3ca7ff6
Rodrigo Gai <rcrosse@walmart.com>
2015-06-24 17:22:39 -0300

    Fix Send submit event

--- app/controllers/poll_controller.rb
+++ app/controllers/poll_controller.rb
@@ -1,12 +1,12 @@
 class PollController < ApplicationController
+  before_action :random_suggestion_poll, only: [:index, :create]
+
   def index
   	@poll = Poll.new
-
-  	@suggestion_poll = Poll.random_suggestion
   end
 
   def create
-  	if Poll.new(poll_params).create
+  	if Poll.new(poll_params).save
   		render 'thanks'
   	end
   end
@@ -14,6 +14,10 @@ class PollController < ApplicationController
   private
 
   def poll_params
-	params.require(:poll).permit(:what_youd_do, :why_youd_do)
+	  params.require(:poll).permit(:what_youd_do, :why_youd_do)
+  end
+
+  def random_suggestion_poll
+    @suggestion_poll = Poll.random_suggestion
   end
 end
